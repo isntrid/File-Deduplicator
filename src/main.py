c5c79d57f57c8ece75df2b72ec042ca9c7faa25f
isntrid <ridn123321@gmail.com>
2026-07-10 22:36:16 +0100

    init

--- src/main.py
+++ src/main.py
@@ -1,20 +1,38 @@
 from pathlib import Path
 
-def get_input():
+class File:
+    def __init__(self, size, modified_time, creation_time):
+        self.size = size
+        self.modified_time = modified_time
+        self.creation_time = creation_time
+
+    def __repr__(self):
+        return f"size={self.size}, modified={self.modified_time}, created={self.creation_time})"
+
+    def __eq__(self, other):
+        return self.size == other.size and self.modified_time == other.modified_time and self.creation_time == other.creation_time
+
+def set_directory():
     home = Path.home()
     while True:
-        location = input("Enter a directory: ")
+        #location = input("Enter a directory: ")
+        location = "downloads" #temp
         directory = home / location
         if directory.is_dir():
-            return directory
+            path = directory.resolve()
         else:
-            print("Directory not found, did you spell it correctly?")
+            print("Directory not found; did you spell it correctly?")
+
+def convert_files():
+    files = list(dir.iterdir())
+    for file in files:
+        path = file.stat()
+        size = path.st_size, modified_time = path.st_mtime, creation_time = path.st_ctime
+        File(size=size, modified_time=modified_time, creation_time=creation_time)
+
 
 
 def main():
     pass
 
-dir = get_input()
-p = list(dir.iterdir())[0]
 
-print(p.stat())
